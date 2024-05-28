e2c9075b22825643a50f8d0173988d1ee9f8f033
Lydia Konstanski <lydiakonstanski@MacBook-Pro.local>
2024-05-28 19:50:07 -0400

    Push prior to styling

--- api_key.js
+++ api_key.js
@@ -1,14 +1,14 @@
-esriConfig.apiKey = "<AAPK2783f16d57c84b76b9d4a4a6e91c2e7bx1JoRgkjG9wcDuYVbycyoQ-shvVHmFt2mHjmb8xCiuvmIt807A-BhuLGkA4-MDh7>";
-const map = new Map({
-    basemap: "arcgis-topographic" // Basemap layer
-});
-
-const view = new MapView({
-    map: map,
-    center: [-118.805, 34.027],
-    zoom: 13, // scale: 72223.819286
-    container: "viewDiv",
-    constraints: {
-        snapToZoom: false
-    }
-});
+// esriConfig.apiKey = "<AAPK2783f16d57c84b76b9d4a4a6e91c2e7bx1JoRgkjG9wcDuYVbycyoQ-shvVHmFt2mHjmb8xCiuvmIt807A-BhuLGkA4-MDh7>";
+// const map = new Map({
+//     basemap: "arcgis-topographic" // Basemap layer
+// });
+//
+// const view = new MapView({
+//     map: map,
+//     center: [-118.805, 34.027],
+//     zoom: 13, // scale: 72223.819286
+//     container: "viewDiv",
+//     constraints: {
+//         snapToZoom: false
+//     }
+// });
